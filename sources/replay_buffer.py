41bfebe64336e720b0d82d605e80b7adaa5ff7ef
GL <GL@wireless-10-145-253-55.public.utexas.edu>
2019-11-17 16:32:10 -0600

    Replay buffer with comments

--- sources/replay_buffer.py
+++ sources/replay_buffer.py
@@ -3,7 +3,7 @@ import numpy as np
 class replay_buffer():
     
     def __init__(self,
-                 buffer_size:int,
+                 buffer_size:int
                  ):
                  
         self.buffer_size = buffer_size
@@ -16,21 +16,30 @@ class replay_buffer():
               img_prev:np.array,
               action:int,
               reward:float,
-              img_next:np.array
+              img_next:np.array,
+              done:bool
               ):
               
+        """
+        Each call of this method stores a 5-element tuple into the replay buffer: current processed image stack, current action taken, reward received after taking the action, next processed image stack and a flag indicates whether the episode terminates
+        """
+              
         if self.stored_samples < self.buffer_size:
-            self.buffer.append((img_prev, action, reward, img_next))
+            self.buffer.append((img_prev, action, reward, img_next, done))
             self.stored_samples += 1
             self.store_ptr = (self.store_ptr + 1) % self.buffer_size
-        else if self.stored_samples == self.buffer_size:
-            self.buffer[self.store_ptr] = (img_prev, action, reward, img_next)
+        elif self.stored_samples == self.buffer_size:
+            self.buffer[self.store_ptr] = (img_prev, action, reward, img_next, done)
             self.store_ptr = (self.store_ptr + 1) % self.buffer_size
         else:
             self.stored_samples = self.buffer_size
             
     def sample(self,
                batch_size: int) -> np.array:
+               
+        """
+        This method returns a numpy array, with batch_size # of elements randomly sampled from replay buffer. Each element of the array is a tuple, consists of current processed image stack, action, reward, next processed image stack and a flag
+        """
                
         if batch_size >= self.stored_samples:
             return np.array(self.buffer)
