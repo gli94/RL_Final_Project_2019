251bb8a4ead68cd14d1bcd687ccdcdda6c1ab094
wshi <weishi0079@gmail.com>
2019-11-17 17:12:41 -0600

    regularize style

--- src/deep_q_learning.py
+++ src/deep_q_learning.py
@@ -1,2 +1,43 @@
 # This is a simplified version of deep Q learning algorithm from the Nature paper "Human-level control through deep
 # reinforcement learning"
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import gym
+from dqn import DQN
+
+env = gym.make('CartPole-v0')
+
+# Initialize the behavior policy
+pi = eplsilon_greedy()
+
+# Initialize experience replay buffer
+
+# Initialize the targetNet and evalNet
+    Q = DQN()
+
+    for episode in range(1, num_episode):
+        x = env.reset()
+        s = [x]
+
+        for t in range(1, T)
+            a = pi(phi(s), DQN.evalNet)
+            x, r, done, _ = env.step(a)
+            phi_t = phi(s)
+            s.append(a, x)
+            phi_t+1 = phi(s)
+
+            buffer.store(phi_t, a, r, phi_t+1)
+            transitionBatch = buffer.sample
+
+            y = []
+            for state in stateBatch:
+                if(transition.phi_j+1 == termination):
+                    y_j = transition.r_j
+                else:
+                    y_j = r_j + gamma * np.argmax(Q.targetNet(phi_j+1))
+
+                y.add(y_j)
+
+            Q.update(stateBatch, y)
--- src/dqn.py
+++ src/dqn.py
@@ -5,21 +5,32 @@ import torch.nn.functional as F
 import numpy as np
 import gym
 
-class Net(nn.module)
-    def __init__(self, state_dim, num_action):
+class Net(nn.module):
+    def __init__(self,
+                 state_dim=(84, 84, 4),
+                 num_action=18):
+        """
+        state_dim: nn input dimension which is (84, 84, 4)
+        num_action: number of possible actions, 4 - 18 for Atari
+        """
+
         super(Net, self).__init__()
         self.fc1 = nn.linear(state_dim, 10)
         self.fc1.weight.data.normal(0, 0.1)
         self.out = nn.Linear(10, num_action)
         self.out.weight.data.normal(0, 0.1)
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
-        action_values = self.out(x)
+    def forward(self,
+                phi: torch.Tensor):
+        """
+        phi: nn input dimension, shape being [84, 84, 4]
+        """
+        phi = self.fc1(phi)
+        phi = F.relu(phi)
+        action_values = self.out(phi)
         return action_values
 
-class DQN(object)
+class DQN(object):
     def __init__(self, state_dim, num_action, alpha, C):
         self.targetNet = Net(state_dim, num_action)
         self.evalNet = Net(state_dim, num_action)
@@ -30,20 +41,22 @@ class DQN(object)
         self.optimizer = torch.optim.Adam(self.evalNet.parameters(), lr=alpha)
         self.loss_func = nn.MSELoss()
 
-    def eval(self, state):
-        state = torch.unsqueeze(torch.FloatTensor(state), 0)
-        return self.evalNet(state)
+    def eval(self, phi):
+        phi = torch.unsqueeze(torch.FloatTensor(phi), 0)
+        return self.evalNet(phi)
 
-    def target(self, state):
-        state = torch.unsqueeze(torch.FloatTensor(state), 0)
-        return self.targetNet(state)
+    def target(self, phi):
+        phi = torch.unsqueeze(torch.FloatTensor(phi), 0)
+        return self.targetNet(phi)
 
-    def update(self, stateBatch, targetBatch):
+    def update(self,
+               phiBatch: torch.Tensor,
+               targetBatch: torch.Tensor):
         if self.learnCounter == 0:
             self.targetNet.load_state_dict(self.evalNet.state_dict())
         self.learnCounter  = (self.learnCounter + 1) % self.C
 
-        prediction = self.evalNet(stateBatch)
+        prediction = self.evalNet(phiBatch)
         loss = self.loss_func(prediction, targetBatch)
         self.optimizer.zero_grad()
         loss.backward()
