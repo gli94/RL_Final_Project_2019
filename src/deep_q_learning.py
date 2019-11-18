3a232121692a402528defc15b4429f6a8ca8aef6
wshi <weishi0079@gmail.com>
2019-11-17 20:14:22 -0600

    updating

--- src/deep_q_learning.py
+++ src/deep_q_learning.py
@@ -6,8 +6,15 @@ import torch.nn.functional as F
 import numpy as np
 import gym
 from src.dqn import DQN
+from sources.replay_buffer import replay_buffer
 
 env = gym.make('CartPole-v0')
+
+# Hyper Parameters
+num_episode = 400
+
+# Initialize the pre-processing function phi
+phi = Phi()
 
 # Initialize the behavior policy
 pi = epsilon_greedy()
@@ -15,29 +22,29 @@ pi = epsilon_greedy()
 # Initialize experience replay buffer
 
 # Initialize the targetNet and evalNet
-    Q = DQN()
+Q = DQN()
 
-    for episode in range(1, num_episode):
-        x = env.reset()
-        s = [x]
+for episode in range(1, num_episode):
+    x = env.reset()
+    s = [x]
 
-        for t in range(1, T)
-            a = pi(phi(s), DQN.evalNet)
-            x, r, done, _ = env.step(a)
-            phi_t = phi(s)
-            s.append(a, x)
-            phi_t+1 = phi(s)
+    for t in range(1, T)
+        a = pi(phi(s), DQN.evalNet)
+        x, r, done, _ = env.step(a)
+        phi_t = phi(s)
+        s.append(a, x)
+        phi_t+1 = phi(s)
 
-            buffer.store(phi_t, a, r, phi_t+1)
-            transitionBatch = buffer.sample
+        buffer.store(phi_t, a, r, phi_t+1)
+        transitionBatch = buffer.sample
 
-            y = []
-            for state in stateBatch:
-                if(transition.phi_j+1 == termination):
-                    y_j = transition.r_j
-                else:
-                    y_j = r_j + gamma * np.argmax(Q.targetNet(phi_j+1))
+        y = []
+        for state in stateBatch:
+            if(transition.phi_j+1 == termination):
+                y_j = transition.r_j
+            else:
+                y_j = r_j + gamma * np.argmax(Q.targetNet(phi_j+1))
 
-                y.add(y_j)
+            y.add(y_j)
 
-            Q.update(stateBatch, y)
+        Q.update(stateBatch, y)
